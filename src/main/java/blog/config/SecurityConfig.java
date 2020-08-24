b34f7ce02c762518788a43af538cc3614d46f298
Jordan Morgan <jordanreedmorgan@gmail.com>
2020-08-24 12:08:45 -0400

    d

--- src/main/java/blog/config/SecurityConfig.java
+++ src/main/java/blog/config/SecurityConfig.java
@@ -53,16 +53,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	    }
 	    
 	    
-	    @Bean
-	    CorsConfigurationSource corsConfigurationSource() {
-	        CorsConfiguration configuration = new CorsConfiguration();
-	        configuration.setAllowedOrigins(Arrays.asList("*"));
-	        configuration.setAllowedMethods(Arrays.asList("*"));
-	        configuration.setAllowedHeaders(Arrays.asList("*"));
-	        configuration.setAllowCredentials(true);
-	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	        source.registerCorsConfiguration("/**", configuration);
-	        return source;
-	    }
+//	    @Bean
+//	    CorsConfigurationSource corsConfigurationSource() {
+//	        CorsConfiguration configuration = new CorsConfiguration();
+//	        configuration.setAllowedOrigins(Arrays.asList("*"));
+//	        configuration.setAllowedMethods(Arrays.asList("*"));
+//	        configuration.setAllowedHeaders(Arrays.asList("*"));
+//	        configuration.setAllowCredentials(true);
+//	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//	        source.registerCorsConfiguration("/**", configuration);
+//	        return source;
+//	    }
 	    
 	}
